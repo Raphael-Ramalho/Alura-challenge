4a59e154e6a6d992ea88f050174988a745dde16f
Raphael Ramalho <rsramalhovf@gmail.com>
2021-06-01 21:08:53 -0300

    update

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+function showHiddenMenu(){
+    const x = document.getElementById("");
+    if (x.style.display === "none"){
+        x.style.display = "fixed";
+    } else {
+        x.style.display = "none";
+    }
+}
+
