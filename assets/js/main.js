0a2e539af59f453d361d32d20fd5ac29a1f5b8f5
Raphael Ramalho <rsramalhovf@gmail.com>
2021-06-02 21:33:03 -0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,29 @@
-function showHiddenMenu(){
-    const x = document.getElementById("");
-    if (x.style.display === "none"){
-        x.style.display = "fixed";
-    } else {
-        x.style.display = "none";
-    }
-}
+// function showHiddenMenu(){
+//     const x = document.getElementById("");
+//     if (x.style.display === "none"){
+//         x.style.display = "fixed";
+//     } else {
+//         x.style.display = "none";
+//     }
+// }
 
+
+
+
+
+// const surroundColor = document.querySelector(["data-textarea-external-color"]);
+
+// const colorFromImput = document.querySelector(["data-color-input"]).value;
+
+// surroundColor.style.backgroundColor = "lightblue";
+
+const colorFromImput = document.querySelector(["data-color-input"]).value;
+
+function SurroundColor() {
+
+const colorFromImput = document.querySelector(["data-color-input"]).value;
+
+console.log(`${colorFromImput}`);
+console.log("hey")
+
+}
