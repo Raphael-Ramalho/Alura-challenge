ad59da67772c5fcbf360d92eb0f668c8dc15c25e
Raphael Ramalho <rsramalhovf@gmail.com>
2021-06-04 21:58:54 -0300

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,3 +8,27 @@ function SurroundColor() {
 }
 
 
+const areaDoCodigo = document.querySelector("[data-codigo-wrapper]");
+const linguagem = document.querySelector("[data-linguagens]");
+const botao = document.querySelector("[data-botao-highlight]");
+
+
+
+function mudaLinguagem() {
+    const codigo = areaDoCodigo.querySelector("[data-textarea-code]");
+    console.log(codigo)
+    areaDoCodigo.innerHTML = `<textarea class = "sessao-principal__container__campo__code hljs ${linguagem.value}" data-textarea-code contenteditable="true"></textarea>`;
+    console.log(areaDoCodigo)
+    areaDoCodigo.firstChild.innerText = codigo.innerText;
+}
+
+linguagem.addEventListener("change", () => {
+    mudaLinguagem();
+})
+
+botao.addEventListener("click", () => {
+    const codigo = areaDoCodigo.querySelector("code");
+    hljs.highlightBlock(codigo);
+})
+
+
